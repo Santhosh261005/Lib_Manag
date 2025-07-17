04f0caa8374b4d827c1cef1a49c0a5ecebfd4165
Santhosh261005 <santhoshreddyjakku@gmail.com>
2025-07-17 15:28:05 +0530

    Server Setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,31 @@
-Hello
+require('dotenv').config();
+
+
+const express = require('express');
+const app = express();
+
+//const notFoundMiddleware = require('./middleware/not-found');
+//const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+
+app.use(express.json());
+
+// Define routes here..before the error handlers
+
+//app.use(notFoundMiddleware);
+//app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
